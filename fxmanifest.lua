164424f1265c161daafc4bd1231be0d28e147ad1
Dallefar <94932668+math32d1@users.noreply.github.com>
2024-02-27 21:21:20 +0100

    fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,19 @@ fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
 
+author 'Sigge x Dallefar'
+description 'Postbud script af Sigge oversæt til vrp af dallefar.'
 version '1.2.0'
+
+shared_scripts {
+  '@ox_lib/init.lua',
+  "Config.lua",
+}
 
 server_scripts {
   "server.lua",
-  '@oxmysql/lib/MySQL.lua',
 }
 
 client_scripts {
   "client.lua",
-  "Config.lua",
-}
-
-shared_scripts {
-    '@ox_lib/init.lua',
 }
